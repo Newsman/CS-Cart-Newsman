56f2c525813a2bffb3c01ffe26509ec928d831c4
lucianfcorodeanu@gmail.com <lucianfcorodeanu@gmail.com>
2022-02-14 13:00:09 +0200

    new controller

---
+++ app/addons/newsman/controllers/frontend/profiles.post.php
@@ -0,0 +1,44 @@
+<?php
+
+use Tygh\Registry;
+use Tygh\BlockManager\ProductTabs;
+use Tygh\Settings;
+
+if (!defined('BOOTSTRAP')) { die('Access denied'); }
+
+$vars = Registry::get('addons.newsman');
+
+$_h = array(
+ $vars["newsman_remarketinglblone"],
+ $vars["newsman_remarketinglbltwo"],
+ $vars["newsman_remarketinglblthree"],
+ $vars["newsman_remarketinglblfour"],
+ $vars["newsman_remarketinglblfive"]
+);
+
+$_hosts = array(
+(!empty($vars["newsman_remarketinglblone"]) ? $vars["newsman_remarketinglblone"] : null) => (!empty($vars["newsman_remarketingone"]) ? $vars["newsman_remarketingone"] : null),
+(!empty($vars["newsman_remarketinglbltwo"]) ? $vars["newsman_remarketinglbltwo"] : null) => (!empty($vars["newsman_remarketingtwo"]) ? $vars["newsman_remarketingtwo"] : null),
+(!empty($vars["newsman_remarketinglblthree"]) ? $vars["newsman_remarketinglblthree"] : null) => (!empty($vars["newsman_remarketingthree"]) ? $vars["newsman_remarketingthree"] : null),
+(!empty($vars["newsman_remarketinglblfour"]) ? $vars["newsman_remarketinglblfour"] : null) => (!empty($vars["newsman_remarketingfour"]) ? $vars["newsman_remarketingfour"] : null),
+(!empty($vars["newsman_remarketinglblfive"]) ? $vars["newsman_remarketinglblfive"] : null) => (!empty($vars["newsman_remarketingfive"]) ? $vars["newsman_remarketingfive"] : null)
+);
+
+$remarketingid = "";
+$host = "";
+
+foreach($_hosts as $h => $v)
+{
+	if(getenv('HTTP_HOST') == $h)
+	{	
+		if(empty($v))
+			continue;
+
+		$remarketingid = $v;
+		$host = $h;
+		break;
+	}
+}
+
+Tygh::$app['view']->assign('newsmanRemarketingEnabled', (!empty($vars["newsman_remarketingenable"])) ? $vars['newsman_remarketingenable'] : "0");
+Tygh::$app['view']->assign('newsmanRemarketingId', $_hosts[$h]);
